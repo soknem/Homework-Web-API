61a3119faf471142a691ef53f290496372da5fc7
soknem <povsoknem@gmail.com>
2025-02-25 13:40:35 +0700

    Add project files.

---
+++ Homework/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Homework.dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework.Controllers
+{
+
+    [ApiController]
+    [Route("/api/v1/homework")]
+    public class HomeController : Controller
+    {
+        [HttpGet("current-time")]
+        public DateTime GetCurrentTime()
+        {
+            return DateTime.Now;
+        }
+
+        [HttpGet("current-age")]
+        public int GetCurrentAge()
+        {
+
+            DateTime dob = new DateTime(2002, 02, 14);
+            return DateTime.Now.Year - dob.Year;
+        }
+
+        [HttpGet("random-number")]
+        public int GetRandomNumber()
+        {
+            return new Random().Next();
+        }
+
+        [HttpPost("transform-number")]
+        public int TransfomNumber([FromBody] NumberDto numberDto)
+        {
+            return numberDto.number;
+        }
+
+    }
+}
---
+++ Homework/dto/NumberDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homework.dto
+{
+    public record NumberDto
+    {
+        [Range(0, int.MaxValue, ErrorMessage = "Id must be positive or zero.")]
+        public int number { get; init; }
+    }
+}
